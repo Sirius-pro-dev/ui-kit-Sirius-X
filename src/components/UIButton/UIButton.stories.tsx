64d45d099c505a330fae5f062d7bb0a536c6da33
Naziser <you@example.com>
2023-10-08 22:43:48 +0300

    feat: add UIButton

---
+++ src/components/UIButton/UIButton.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import UIButton from './UIButton';
+
+export default {
+  title: 'ReactComponentLibrary/UIButton',
+  component: UIButton,
+} as ComponentMeta<typeof UIButton>;
+
+const Template: ComponentStory<typeof UIButton> = (args) => <UIButton {...args} />;
+
+export const UIButtonInstance = Template.bind({});
+UIButtonInstance.args = {
+  label: 'Подключиться',
+};
---
+++ src/components/UIButton/UIButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./UIButton.scss";
+
+export interface UIButtonProps {
+  label: string;
+  disabled?: boolean;
+}
+
+const UIButton = (props: UIButtonProps) => {
+  return (
+    <button className="button__wrapper" disabled={props.disabled}>
+      <span className="button__text">{props.label}</span>
+    </button>
+  );
+};
+
+export default UIButton;
