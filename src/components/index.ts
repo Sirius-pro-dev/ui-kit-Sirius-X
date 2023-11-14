bab613da0b7471d3fbda8a596ca6bfb4dcf59d2e
Kuzy53 <91885270+Kuzy53@users.noreply.github.com>
2023-11-14 19:16:08 +0300

    checkBox edit (#6)
    Co-authored-by: kuzy53 <nikita129343@mail.ru>

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,6 @@ export { default as Button } from './Button';
 export { default as Loader } from './Loader';
 export { default as UIButton } from './UIButton';
 export { default as UIFormRegistration } from './UIFormRegistration';
+export { default as UICheckBox } from './UICheckBox';
 export { default as UIModal } from './UIModal';
 export { default as UIFormInput } from './UIFormInput';
-
