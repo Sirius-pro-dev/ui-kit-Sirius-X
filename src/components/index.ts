6e045836205396d060b24c67e544e20b7f687b1c
Kuzy53 <91885270+Kuzy53@users.noreply.github.com>
2023-11-14 19:13:43 +0300

    modal edit (#8)
    Co-authored-by: kuzy53 <nikita129343@mail.ru>

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ export { default as Button } from './Button';
 export { default as Loader } from './Loader';
 export { default as UIButton } from './UIButton';
 export { default as UIFormRegistration } from './UIFormRegistration';
+export { default as UIModal } from './UIModal';
 export { default as UIFormInput } from './UIFormInput';
 
