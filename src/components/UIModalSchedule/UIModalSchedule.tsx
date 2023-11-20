df045a6c64ae2abeb07d381c105e90d1093c2cb3
dancinginthedarkk <m.angellina@yandex.ru>
2023-11-21 01:48:42 +0300

    feat: add UIModalSchedule

--- src/components/UIModalSchedule/UIModalSchedule.tsx
+++ src/components/UIModalSchedule/UIModalSchedule.tsx
@@ -5,17 +5,44 @@ import Vector from './Vector.svg';
 export interface ModalScheduleProps {
   title: string;
   subtitle?: string[];
-  data: (string | undefined)[];
+  data: {
+    dataDay?: {
+      day?: string;
+    };
+    time?: string;
+    name?: string;
+    classType?: string;
+    teacher?: string;
+    placeActivity?: string;
+    classRoom?: string;
+    group?: {
+      name?: string;
+    };
+  };
 }
 
-const defaultSubtitle = ['Дата', 'Время', 'Дисциплина', 'Тип', 'Преподаватель', 'Адрес', 'Аудитория / URL', 'Группа']
+const defaultSubtitle = ['День', 'Время', 'Дисциплина', 'Тип', 'Преподаватель', 'Адрес', 'Аудитория', 'Группа']
 
 const ModalSchedule = ({ title, subtitle = defaultSubtitle, data }: ModalScheduleProps) => {
   const [isOpen, setIsOpen] = useState(true);
 
+  const { dataDay, time, name, classType, teacher, placeActivity, classRoom, group } = data;
+
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
+
+  const dataMapping = {
+    'День': dataDay?.day || '-',
+    'Время': time || '-',
+    'Дисциплина': name || '-',
+    'Тип': classType || '-',
+    'Преподаватель': teacher || '-',
+    'Адрес': placeActivity || '-',
+    'Аудитория': classRoom || '-',
+    'Группа': group?.name || '-',
+  };
+
 
   return (
       <div className={`modal-container ${isOpen ? 'open' : ''}`}>
@@ -24,12 +51,12 @@ const ModalSchedule = ({ title, subtitle = defaultSubtitle, data }: ModalSchedul
             <p className='modal__title'>{title}</p>
             <img className='modal__close-icon' src={Vector} alt={'close-icon'} onClick={toggleModal} />
           </div>
-          <div className='modal__info-wrapper'>
+          <div className="modal__info-wrapper">
             {subtitle.map((sub, index) => (
-                <div key={index} className='modal__content'>
-                  <p className='modal__subtitle'>{sub}</p>
-                  <p className='modal__data'>{data[index] || '-'}</p>
-                  {index !== subtitle.length - 1 && <div className='modal__separator' />}
+                <div key={index} className="modal__content">
+                  <p className="modal__subtitle">{sub}</p>
+                  <p className="modal__data">{dataMapping[sub]}</p>
+                  {index !== subtitle.length - 1 && <div className="modal__separator" />}
                 </div>
             ))}
           </div>
