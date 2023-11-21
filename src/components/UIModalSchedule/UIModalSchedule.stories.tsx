f267100a420e06a76ce54e9c8ad6f0d2c9c9b757
dancinginthedarkk <91607846+dancinginthedarkk@users.noreply.github.com>
2023-11-21 17:51:20 +0300

    UIModalSchedule (#9)
    * feat: add UIModalSchedule

---
+++ src/components/UIModalSchedule/UIModalSchedule.stories.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import UIModalSchedule from './UIModalSchedule';
+
+export default {
+  title: 'ReactComponentLibrary/UIModalSchedule',
+  component: UIModalSchedule,
+} as ComponentMeta<typeof UIModalSchedule>;
+
+const Template: ComponentStory<typeof UIModalSchedule> = (args) => (
+  <UIModalSchedule {...args} />
+);
+
+export const UIModalScheduleInstance = Template.bind({});
+const data = {
+    "lessonKey": "0",
+    "dataDay": {
+        "date": "20",
+        "day": "ПН",
+        "lessons": [
+            {
+                "time": "09:00-10:30",
+                "name": "Алгоритмы",
+                "classType": "Лекция",
+                "placeActivity": "ГМЦ",
+                "teacher": "Денис Сергеевич Степанов",
+                "classRoom": "004",
+                "group": {
+                    "_id": "654a77c484b69067b242ed02",
+                    "name": "K025-1",
+                    "major": "Информационные технологии и программирование",
+                    "course": 2,
+                    "studyForm": "Очная",
+                    "educationLevel": "СПО",
+                    "users": [
+                        "652a549bf151b9c8da2cfe6d"
+                    ],
+                    "__v": 0
+                }
+            },
+            {
+                "time": "10:45-12:15",
+                "name": "Базы данных",
+                "classType": "Семинар",
+                "placeActivity": "ГМЦ",
+                "teacher": "Денис Сергеевич Степанов",
+                "classRoom": "213",
+                "group": {
+                    "_id": "654a77c484b69067b242ed02",
+                    "name": "K025-1",
+                    "major": "Информационные технологии и программирование",
+                    "course": 2,
+                    "studyForm": "Очная",
+                    "educationLevel": "СПО",
+                    "users": [
+                        "652a549bf151b9c8da2cfe6d"
+                    ],
+                    "__v": 0
+                }
+            },
+            {
+                "time": "15:00-16:30",
+                "name": "Технология разработки программного обеспечения",
+                "classType": "Экзамен",
+                "placeActivity": "ГМЦ",
+                "teacher": "Денис Сергеевич Степанов",
+                "classRoom": "К_7",
+                "group": {
+                    "_id": "654a77c484b69067b242ed02",
+                    "name": "K025-1",
+                    "major": "Информационные технологии и программирование",
+                    "course": 2,
+                    "studyForm": "Очная",
+                    "educationLevel": "СПО",
+                    "users": [
+                        "652a549bf151b9c8da2cfe6d"
+                    ],
+                    "__v": 0
+                }
+            }
+        ]
+    },
+    "time": "09:00-10:30",
+    "name": "Алгоритмы",
+    "classType": "Лекция",
+    "placeActivity": "ГМЦ",
+    "teacher": "Денис Сергеевич Степанов",
+    "classRoom": "004",
+    "group": {
+        "_id": "654a77c484b69067b242ed02",
+        "name": "K025-1",
+        "major": "Информационные технологии и программирование",
+        "course": 2,
+        "studyForm": "Очная",
+        "educationLevel": "СПО",
+        "users": [
+            "652a549bf151b9c8da2cfe6d"
+        ],
+        "__v": 0
+    }
+}
+UIModalScheduleInstance.args = {
+    title: 'Информация о событии',
+    data: data
+};
---
+++ src/components/UIModalSchedule/UIModalSchedule.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './UIModalSchedule.scss';
+import Vector from './Vector.svg';
+
+export interface ModalScheduleProps {
+  title: string;
+  subtitle?: string[];
+  data: {
+    dataDay?: {
+      day?: string;
+    };
+    time?: string;
+    name?: string;
+    classType?: string;
+    teacher?: string;
+    placeActivity?: string;
+    classRoom?: string;
+    group?: {
+      name?: string;
+    };
+  };
+}
+
+const defaultSubtitle = ['День', 'Время', 'Дисциплина', 'Тип', 'Преподаватель', 'Адрес', 'Аудитория', 'Группа']
+
+const ModalSchedule = ({ title, subtitle = defaultSubtitle, data }: ModalScheduleProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const { dataDay, time, name, classType, teacher, placeActivity, classRoom, group } = data;
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const dataMapping = {
+    'День': dataDay?.day || '-',
+    'Время': time || '-',
+    'Дисциплина': name || '-',
+    'Тип': classType || '-',
+    'Преподаватель': teacher || '-',
+    'Адрес': placeActivity || '-',
+    'Аудитория': classRoom || '-',
+    'Группа': group?.name || '-',
+  };
+
+
+  return (
+      <div className={`modal-container ${isOpen ? 'open' : ''}`}>
+        <div className='modal-wrapper'>
+          <div className='modal__title-container'>
+            <p className='modal__title'>{title}</p>
+            <img className='modal__close-icon' src={Vector} alt={'close-icon'} onClick={toggleModal} />
+          </div>
+          <div className="modal__info-wrapper">
+            {subtitle.map((sub, index) => (
+                <div key={index} className="modal__content">
+                  <p className="modal__subtitle">{sub}</p>
+                  <p className="modal__data">{dataMapping[sub]}</p>
+                  {index !== subtitle.length - 1 && <div className="modal__separator" />}
+                </div>
+            ))}
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default ModalSchedule;
